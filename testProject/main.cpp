952baaa2799029e5090837f197c9c1f0d85fae53
hmkyriacou <hmkyriacou14@gmail.com>
2020-03-05 13:08:03 -0500

    Initial Commit

---
+++ testProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testProject
+//
+//  Created by Harrison Kyriacou on 3/5/20.
+//  Copyright © 2020 Harrison Kyriacou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
